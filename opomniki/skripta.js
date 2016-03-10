4f331fccc464c8ee33984f8e6f0ad113a03cae41
Regina Polc <rp0223@student.uni-lj.si>
2016-03-10 11:24:08 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -8,6 +8,21 @@ window.addEventListener('load', function() {
 		document.querySelector(".pokrivalo").style.visibility="hidden";
 	}
 	document.querySelector("#prijavniGumb").addEventListener("click", izvediPrijavo);
+	
+	//dodaj opomnike
+	var dodajOpomnik=function(){
+		var naziv_opomnika=document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika=document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value= "";
+		document.querySelector("#cas_opomnika").value= "";
+		document.querySelector("#opomniki").innerHTML+= "\
+			<div class='opomnik rob senca'>"+ naziv_opomnika +"</div>\
+			<div class='cas_opomnika'>Opomnik čez <span>"+cas_opomnika+
+				"</span> sekund</div></div>";
+			
+			
+	}
+	document.querySelector("#dodajGumb").addEventListener("click", dodajOpomnik);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
